a1df83ead99294bd508e9de46a19b1def6c09edf
최신형 <tlsgudrhfem@naver.com>
2022-04-22 14:56:56 +0900

    로컬 테이블 스페이스 추가

--- db_pro01/Scripts/Script01_Local.sql
+++ db_pro01/Scripts/Script01_Local.sql
@@ -2,11 +2,11 @@
 SELECT 'HELLO Oracle' FROM DUAL;
 
 /*
- * 사용자(user)가 local에 구성한 사람은 system 계정을 사용중. - DBMS 관리자 계정
  * 일반 사용자 계정을 새로 만들어 관리자 계정과 분리.
+ * 아래의 작업은 관리자 권한이 있는 계정을 사용해야 함.
  */
 
--- 계정과 패스워드 생성하는 방법
+-- 계정과 패스워드 생성하는 방법(생성 - CREATE, 변경 - ALTER)
 CREATE USER puser1 IDENTIFIED BY puser1;
 
 /*
@@ -19,3 +19,29 @@ GRANT RESOURCE, CONNECT TO puser1;
 GRANT INSERT ANY TABLE, UPDATE ANY TABLE
 	, DELETE ANY TABLE, CREATE VIEW
  TO puser1;
+
+ 
+-- 마우스 우클릭 - 실행 - 활성화된 DB연결 설정
+
+-- 테이블스페이스 사용 권한 부여
+ALTER USER puser1 quota 10M ON USERS;
+COMMIT;
+ 
+-- 사용자 계정 목록 확인하는 법.(USERNAME = 컬럼명)
+SELECT USERNAME FROM ALL_USERS;
+
+-- 계정에 부여한 권한 확인 (관리자 계정)
+SELECT PRIVILEGE FROM DBA_SYS_PRIVS WHERE GRANTEE = 'PUSER1';
+
+-- 계정에 부여한 ROLE 권한 확인 (관리자 계정)
+SELECT * FROM DBA_ROLE_PRIVS WHERE GRANTEE = 'PUSER1';
+
+COMMIT;
+
+
+
+
+
+
+
+
