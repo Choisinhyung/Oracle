ed2a1b621c863f299e79883a7204e3bad6eeedd3
최신형 <tlsgudrhfem@naver.com>
2022-04-30 00:57:23 +0900

    수정

--- db_pro02/Scripts/Script01.sql
+++ db_pro02/Scripts/Script01.sql
@@ -40,23 +40,22 @@ LOB : LARGE OBJECT Byte DATA
 -- NOT NULL, UNIQUE
 
 CREATE TABLE sample_t (
-       u_id     NUMBER       PRIMARY KEY-- 컬럼 레벨 
-     , jumin    CHAR(13)     UNIQUE
+       u_id     NUMBER       --PRIMARY KEY-- 컬럼 레벨 
+     , jumin    CHAR(13)     --UNIQUE
 	 , name     VARCHAR2(50) NOT NULL
 	 , age      NUMBER(3)    DEFAULT(0)
 	 , gender   CHAR(1)      CHECK(GENDER IN ('M', 'F'))
      , birthday DATE         DEFAULT(SYSDATE)
-     , ref_col  NUMBER       REFERENCES ref_t(r_id)
-     , CONSTRANT PK_U_ID     PRIMARY KEY(u_id)
-     , CONSTRANT UK_JUMIN    UNIQUE(jumin)
-     , CONSTRANT FK_REF_T_R_ID FOREIGN KEY(ref_col) REFERENCES ref_t(r_id)
-
+     , ref_col  NUMBER       --REFERENCES ref_t(r_id)
+     , CONSTRAINT PK_U_ID     PRIMARY KEY(u_id)
+     , CONSTRAINT UK_JUMIN    UNIQUE(jumin)
+     , CONSTRAINT FK_REF_T_R_ID FOREIGN KEY(ref_col) REFERENCES ref_t(r_id)
 );
 -- 외래키 참조 확인하기위해 참조테이블 생성
 CREATE TABLE ref_t (
        r_id NUMBER PRIMARY KEY
      , note VARCHAR2(100)
-); 
+);
 
 DROP TABLE ref_t;
 DROP TABLE sample_t;
@@ -66,7 +65,7 @@ SELECT * FROM USER_ALL_TABLES WHERE TABLE_NAME = 'SAMPLE_T';
 -- SAMPLE_T테이블의 컬럼을 검색
 SELECT * FROM USER_TAB_COLUMNS WHERE TABLE_NAME = 'SAMPLE_T';
 -- 제약조건에 대한 이름을 부여 (안해도 되지만 관리목적 ~ )
-SELECT * FROM USER_CONSTRANTS WHERE TABLE_NAME = 'SAMPLE_T';
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'SAMPLE_T';
 
 -- DESC SAMPLE_T;
 
@@ -80,4 +79,68 @@ SELECT * FROM USER_COL_COMMENTS WHERE TABLE_NAME = 'SAMPLE_T';
 
 -- 테이블 삭제
 DROP TABLE sample_t;
+DROP TABLE MY_TEST;
+DROP TABLE ALT_T;
 
+CREATE TABLE ALT_T(
+	   u_id NUMBER
+	 , name VARCHAR(10)
+	 , g_type CHAR(1)
+	 , now_date DATE
+	 , remark VARCHAR(100)	 
+);
+
+CREATE TABLE MY_TEST_REF(
+       ref_id NUMBER PRIMARY KEY
+     , ref_text VARCHAR(100)
+);
+
+/*
+ * ★테이블 정보를 수정할 때 기존 데이터 유무와 다른 테이블과의 관계에 대한 사항을 고려하여 작성해야 한다.★
+ */
+
+-- 테이블 이름 변경 : 비추천! 참조되어 있는 테이블이 있을 경우 더 이상 참조할 수 없기 때문에 비추
+ALTER TABLE ALT_T RENAME TO MY_TEST;
+
+-- 컬럼 이름 변경 : 이것도 비추 ~ 
+ALTER TABLE MY_TEST RENAME COLUMN now_date TO birth_date;
+
+-- 새로운 컬럼 추가 : DEFAULT는 필수로 작성하는게 좋음. 안하면 NULL이니까!
+ALTER TABLE MY_TEST ADD age NUMBER(3) NOT NULL;
+ALTER TABLE MY_TEST ADD ref_col NUMBER;
+
+-- MODIFY : 이미 있는 컬럼에 대해서 타입을 지정 / 백업하고 새로 만드는게 좋대..ㅜ
+ALTER TABLE MY_TEST MODIFY name VARCHAR(20);
+ALTER TABLE MY_TEST MODIFY age NUMBER(3) DEFAULT(0);
+
+--데이터의 크기를 늘리는 방향으로 사용하는 것을 추천.
+ALTER TABLE MY_TEST MODIFY name VARCHAR(50);
+
+--컬럼 지우기 : 비추!
+ALTER TABLE MY_TEST DROP COLUMN REMARK;
+
+--제약조건 추가(PRIMARY KEY)
+ALTER TABLE MY_TEST MODIFY u_id NUMBER CONSTRAINT PK_MY_TEST_U_ID PRIMARY KEY; -- 컬럼 레벨로 컬럼 수정하면서 추가
+ALTER TABLE MY_TEST ADD CONSTRAINT PK_MY_TEST_U_ID  PRIMARY KEY(u_id); -- 테이블레벨로추가
+
+ALTER TABLE MY_TEST MODIFY u_id NUMBER;
+
+ALTER TABLE MY_TEST MODIFY name VARCHAR(50) UNIQUE;
+ALTER TABLE MY_TEST ADD UNIQUE(name);
+ALTER TABLE MY_TEST MODIFY ref_col NUMBER REFERENCES MY_TEST_REF(ref_id);
+ALTER TABLE MY_TEST ADD FOREIGN KEY(ref_col) REFERENCES MY_TEST_REF(ref_id);
+
+ALTER TABLE MY_TEST MODIFY g_type NOT NULL;
+ALTER TABLE MY_TEST MODIFY age CHECK(AGE BETWEEN 0 AND 150);
+--제거
+ALTER TABLE MY_TEST DROP CONSTRAINT SYS_C008015;
+
+ALTER TABLE MY_TEST_REF DROP COLUMN ref_id CASCADE CONSTRAINT; -- 비추..
+DROP TABLE MY_TEST_REF CASCADE CONSTRAINT; -- 비추..
+
+--테이블 유무 확인
+SELECT * FROM USER_ALL_TABLES WHERE TABLE_NAME = 'MY_TEST';
+-- 컬럼 확인
+SELECT * FROM USER_TAB_COLUMNS WHERE TABLE_NAME = 'MY_TEST';
+-- 조건 확인
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'MY_TEST';
